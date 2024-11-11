bfcc48653e4211a46e4054f86261435593934924
jakub-jarek <jakub-jarek@users.noreply.github.com>
2024-11-11 12:44:32 +0000

    Initialize Edgio

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+// This file was automatically added by edgio init.
+// You should commit this file to source control.
+import { withEdgio } from '@edgio/next/config/index.js'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+const _preEdgioExport = nextConfig;
+
+export default (_phase, _config) => withEdgio({
+  ..._preEdgioExport
+});
